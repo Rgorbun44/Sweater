22ee3b4cd7a02c003643f23aa3da94b2cad9c01a
Denis.Denutj <Rgorbun44@gmail.com>
2020-02-26 00:28:04 +0300

    Fix filter,freemarker

--- src/main/java/com/denis/sweater/controller/MainController.java
+++ src/main/java/com/denis/sweater/controller/MainController.java
@@ -20,15 +20,23 @@ public class MainController {
     private MessageRepo messageRepo;
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting( Map<String, Object> model) {
         return "greeting";
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam (required = false) String filter,Model model) {
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
 
         return "main";
     }
@@ -50,18 +58,4 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
 }
---
+++ src/main/resources/templates/login.ftl
@@ -0,0 +1,6 @@
+<#import "parts/common.ftl" as c>
+<#import "parts/login.ftl" as l>
+<@c.page>
+    <@l.login "/login" ></@l.login>
+<a href="/registration">Add new user</a>
+</@c.page>
