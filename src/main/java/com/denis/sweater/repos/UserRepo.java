4a8efa759dac311389e375f2aacd2a2d0335a173
Denis.Denutj <Rgorbun44@gmail.com>
2020-02-25 21:53:20 +0300

    first branch

---
+++ src/main/java/com/denis/sweater/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.denis.sweater.repos;
+
+import com.denis.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
