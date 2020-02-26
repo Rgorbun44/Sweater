45e6117f3bde41f1665777fea332d06299c777c0
Denis.Denutj <Rgorbun44@gmail.com>
2020-02-26 21:11:01 +0300

    Upload file

--- src/main/java/com/denis/sweater/controller/MainController.java
+++ src/main/java/com/denis/sweater/controller/MainController.java
@@ -4,20 +4,28 @@ import com.denis.sweater.domain.User;
 import com.denis.sweater.repos.MessageRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.jws.soap.SOAPBinding;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private  String path;
 
     @GetMapping("/")
     public String greeting( Map<String, Object> model) {
@@ -45,9 +53,25 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model
-    ) {
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam ("file")MultipartFile file
+            ) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file != null){
+            File dir = new File(path);
+
+            if(!dir.exists()){
+                dir.mkdir();
+            }
+
+            String uuid = UUID.randomUUID().toString();
+           String resulfileName =  uuid + "." + file.getOriginalFilename();
+
+           file.transferTo(new File(dir + "/" + resulfileName));
+
+            message.setFilename(resulfileName);
+        }
 
         messageRepo.save(message);
 
